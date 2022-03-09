db7bfc5d2dd52b85c3287a2405cf9896510c1bb0
DeathHound <acraig176@hotmail.com>
2022-03-09 20:38:28 +0000

    Fix code for a container-based host

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
 const express = require("express");
 const port = 3003;
 const path = require("path");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Server } = require("http");
-const serverless = require("serverless-http");
 
 const app = express();
-const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cors())
+app.use(cors());
 app.set('views', "views");
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").renderFile);
 app.use(express.static(path.join(__dirname, "public")));
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("../views/index.ejs");
 });
 
-router.get("/home", (req, res) => {
+app.get("/home", (req, res) => {
     res.redirect("/");
 });
 
-router.get("/appeal", (req, res) => {
+app.get("/appeal", (req, res) => {
     res.render("../views/appeal.ejs");
 });
 
-router.get("/apply", (req, res) => {
+app.get("/apply", (req, res) => {
     res.render("../views/apply.ejs");
 });
 
-router.get("/bma-appeal", (req, res) => {
+app.get("/bma-appeal", (req, res) => {
     res.render("../views/bma.ejs");
 });
 
@@ -48,14 +45,10 @@ app.post('/submit', (req, res) => {
     res.send("Work Done");
 });
 
-router.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.status(404).render('../views/404.ejs');
 });
 
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`);
 });
-
-app.use('/.netlify/functions/index', router);
-
-module.exports.handler = serverless(app);
