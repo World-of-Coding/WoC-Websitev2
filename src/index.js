2a67648ba60418e214a6d343943bd88bdf65d25d
DeathHound <acraig176@hotmail.com>
2022-03-10 11:48:29 +0000

    Fix css/images/js displaying

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const port = 3003;
-const path = require("path");
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cors());
 app.set('views', "views");
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").renderFile);
-app.use(express.static(path.join(__dirname, "public")));
+app.set("x-powered-by", false);
+app.use("/public", express.static(`${__dirname}/../public`));
 
 app.get("/", (req, res) => {
     res.render("../views/index.ejs");
